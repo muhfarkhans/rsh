60274887ba216d6c21218773275674c6d4b930e2
Muhammad Farkhan Syafii <zheninjas75@yahoo.co.id>
2025-02-08 21:38:02 +0700

    add filter in the list transactions

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -6,10 +6,15 @@ use App\Constants\TransactionStatus;
 use App\Filament\Resources\TransactionResource\Pages;
 use App\Filament\Resources\TransactionResource\RelationManagers;
 use App\Models\Transaction;
+use Carbon\Carbon;
 use Filament\Forms;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\Indicator;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
@@ -64,11 +69,48 @@ class TransactionResource extends Resource
                         };
                     })
                     ->sortable(),
+                TextColumn::make('created_at')
+                    ->label('Created at')
+                    ->formatStateUsing(function (string $state): string {
+                        $diff = Carbon::parse($state)->diffForHumans();
+                        return __("{$state} ({$diff})");
+                    })
+                    ->searchable()
+                    ->sortable(),
             ])
             ->defaultSort('created_at', 'desc')
             ->filters([
-                //
-            ])
+                Filter::make('created_at')
+                    ->form([
+                        DatePicker::make('created_from'),
+                        DatePicker::make('created_until'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['created_from'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '>=', $date),
+                            )
+                            ->when(
+                                $data['created_until'],
+                                fn(Builder $query, $date): Builder => $query->whereDate('created_at', '<=', $date),
+                            );
+                    })
+                    ->indicateUsing(function (array $data): array {
+                        $indicators = [];
+                        if ($data['created_from'] ?? null) {
+                            $indicators[] = Indicator::make('Created from ' . Carbon::parse($data['created_from'])->toFormattedDateString())
+                                ->removeField('created_from');
+                        }
+                        if ($data['created_until'] ?? null) {
+                            $indicators[] = Indicator::make('Created until ' . Carbon::parse($data['created_until'])->toFormattedDateString())
+                                ->removeField('created_until');
+                        }
+
+                        return $indicators;
+                    })
+                    ->columnSpan(2)->columns(2)
+            ], layout: FiltersLayout::AboveContent)->filtersFormColumns(2)
             ->actions([
                 Tables\Actions\EditAction::make(),
             ])
--- app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
+++ app/Filament/Resources/TransactionResource/Pages/ListTransactions.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\TransactionResource\Pages;
 
+use App\Constants\TransactionStatus;
 use App\Filament\Resources\TransactionResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListTransactions extends ListRecords
 {
@@ -15,5 +18,24 @@ class ListTransactions extends ListRecords
         return [
             //
         ];
+    }
+
+    public function getTabs(): array
+    {
+        $dataStatus = [
+            TransactionStatus::WAITING_FOR_PAYMENT => 'Menunggu pembayaran',
+            TransactionStatus::PAID => 'Lunas',
+            TransactionStatus::CANCEL => 'Dibatalkan',
+        ];
+
+        $tabs = [
+            "Semua" => Tab::make(),
+        ];
+
+        foreach ($dataStatus as $key => $status) {
+            $tabs[$status] = Tab::make()->modifyQueryUsing(fn(Builder $query) => $query->where('status', $key));
+        }
+
+        return $tabs;
     }
 }
